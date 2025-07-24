f9d62aa3bcb6a96affde6019a1484dc0f1cd73c3
Fahim <fahimshariar28@gmail.com>
2025-07-25 00:14:37 +0600

    Implement generateMetadata function to dynamically fetch and set SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { getCourseData } from "@/lib/api";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,11 +13,65 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Best IELTS Preparation Course by Munzereen Shahid [2025]",
-  description:
-    "Get complete preparation of Academic IELTS and General Training IELTS in one course! Join our IELTS Course today to achieve your desired band score under the guidance of the best IELTS Instructor in the country.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const courseData = await getCourseData();
+  return {
+    title: courseData.seo.title,
+    description: courseData.seo.description,
+    keywords: courseData.seo.keywords,
+    openGraph: {
+      title: courseData.seo.defaultMeta.find(
+        (meta) => meta.value === "og:title"
+      )?.content,
+      description: courseData.seo.defaultMeta.find(
+        (meta) => meta.value === "og:description"
+      )?.content,
+      images: [
+        {
+          url:
+            courseData.seo.defaultMeta.find((meta) => meta.value === "og:image")
+              ?.content ?? "",
+          alt: courseData.seo.defaultMeta.find(
+            (meta) => meta.value === "og:image:alt"
+          )?.content,
+          type: courseData.seo.defaultMeta.find(
+            (meta) => meta.value === "og:image:type"
+          )?.content,
+        },
+      ],
+      locale: courseData.seo.defaultMeta.find(
+        (meta) => meta.value === "og:locale"
+      )?.content,
+      url: courseData.seo.defaultMeta.find((meta) => meta.value === "og:url")
+        ?.content,
+    },
+    twitter: {
+      title: courseData.seo.defaultMeta.find(
+        (meta) => meta.value === "og:title"
+      )?.content,
+      description: courseData.seo.defaultMeta.find(
+        (meta) => meta.value === "og:description"
+      )?.content,
+      images: [
+        {
+          url:
+            courseData.seo.defaultMeta.find((meta) => meta.value === "og:image")
+              ?.content ?? "",
+          alt: courseData.seo.defaultMeta.find(
+            (meta) => meta.value === "og:image:alt"
+          )?.content,
+        },
+      ],
+      card: "summary_large_image",
+    },
+    alternates: {
+      canonical:
+        courseData.seo.defaultMeta.find((meta) => meta.value === "og:url")
+          ?.content ?? "",
+    },
+    metadataBase: new URL("https://10minuteschool.com"),
+  };
+}
 
 export default function RootLayout({
   children,
