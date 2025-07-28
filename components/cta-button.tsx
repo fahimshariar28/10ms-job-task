58d86c526716e428419c42e6a1efc02bf2f242a0
Fahim <fahimshariar28@gmail.com>
2025-07-28 17:46:42 +0600

    Add cursor pointer to CtaButton for improved user interaction

--- components/cta-button.tsx
+++ components/cta-button.tsx
@@ -10,7 +10,7 @@ type CtaButtonProps = {
 const CtaButton = (ctaText: CtaButtonProps) => {
   return (
     <div className="flex justify-center my-1 md:my-4 px-3 md:px-5 w-full">
-      <button className="w-full bg-green-600 text-white px-4 py-1 rounded-lg transition hover:bg-green-700 border-b-4 border-green-700">
+      <button className="cursor-pointer w-full bg-green-600 text-white px-4 py-1 rounded-lg transition hover:bg-green-700 border-b-4 border-green-700">
         {ctaText.ctaText.name}
       </button>
     </div>
